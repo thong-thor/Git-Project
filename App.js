6f6cf5637e4a37663667012d60146457a00588b3
thong-thor205N <thongthor205N@gmail.com>
2024-04-08 14:10:58 +0700

    Update App.js to verson3

--- App.js
+++ App.js
@@ -3,3 +3,12 @@ let years=[2020,2021,2022,2023,2024]
 years.map(item=>{
     console.log(item)
 })
+
+const person={
+    name:"thong",
+    age:20,
+    address:"tanmixai",
+    major:"Website and English"
+}
+
+console.log("My name is",person.name,"and now i'm studying",person.major)
